5819023fa46e898062e7110114b98c060d5f9fbd
John S. Urban <urbanjost@comcast.net>
2022-01-03 20:58:08 -0500

    refresh

--- app/today.f90
+++ app/today.f90
@@ -89,7 +89,7 @@ help_text=[ CHARACTER(LEN=128) :: &
 '@(#)DESCRIPTION:    output current time for uses such as file suffixes.>',&
 '@(#)VERSION:        1.0, 2009>',&
 '@(#)AUTHOR:         John S. Urban>',&
-'@(#)COMPILED:       2022-01-02 00:08:15 UTC-300>',&
+'@(#)COMPILED:       2022-01-03 20:28:23 UTC-300>',&
 '']
    WRITE(*,'(a)')(trim(help_text(i)(5:len_trim(help_text(i))-1)),i=1,size(help_text))
    stop ! if --version was specified, stop
