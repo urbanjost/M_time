e4e5b8f47a2c6f04437e1ea216c41cbc73f53c3c
John S. Urban <urbanjost@comcast.net>
2022-05-01 21:58:37 -0400

    CD/CI scripts

--- app/today.f90
+++ app/today.f90
@@ -89,7 +89,7 @@ help_text=[ CHARACTER(LEN=128) :: &
 '@(#)DESCRIPTION:    output current time for uses such as file suffixes.>',&
 '@(#)VERSION:        1.0, 2009>',&
 '@(#)AUTHOR:         John S. Urban>',&
-'@(#)COMPILED:       2022-03-11 16:28:42 UTC-300>',&
+'@(#)COMPILED:       2022-05-01 08:31:41 UTC-240>',&
 '']
    WRITE(*,'(a)')(trim(help_text(i)(5:len_trim(help_text(i))-1)),i=1,size(help_text))
    stop ! if --version was specified, stop
