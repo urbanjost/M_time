2d7ba3630826e07d1367dededf12207abf234e65
urbanjost <urbanjost@comcast.net>
2023-03-26 21:06:56 +0000

    Deploying to gh-pages from  @ 5e7c65d5fe9c18c4550587e66bf87393bc236ef8 🚀

--- docs/fpm-ford/src/demo_box_month.f90
+++
@@ -1,9 +0,0 @@
-           program demo_box_month
-           use M_time, only : box_month
-           implicit none
-           integer           :: dat(8)
-           character(len=21) :: calendar(8)
-              call date_and_time(values=dat)
-              call box_month(dat,calendar)
-              write(*,'(a)')calendar
-           end program demo_box_month
