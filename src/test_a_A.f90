6633d7f84588e850c91798c4e72901d3cee80cb6
urbanjost <urbanjost@comcast.net>
2024-07-16 05:58:43 +0000

    Deploying to gh-pages from  @ 8533c806dbc620e6439f5e16be965ba9f2fad01b 🚀

--- src/test_a_A.f90
+++ src/test_a_A.f90
@@ -6,16 +6,19 @@ integer :: dat(8)
 real(kind=wp) :: a,b
 character(len=:),allocatable :: string
    call date_and_time(values=dat)
+
    string=fmtdate(dat,"%e")//' '//fmtdate(u2d(0),"%A")
    read(string,*)a,b
    if ( nint(b -a ).ne.0) then
-      write(*,*)'%a test:',b,a,b-a
+      write(*,*)'FAILED: %A test:',b,a,b-a
       stop 1
    endif
+
    string=fmtdate(u2d(0),"%a")
    if ( nint(days2sec(string)-b) .ne. 0) then 
-      write(*,*)'%a test:',days2sec(string),b,days2sec(string)-b
+      write(*,*)'FAILED: a% test:',days2sec(string),b,days2sec(string)-b
       stop 2
    endif
-   stop 'tested %a and %A'
+
+   stop 'PASSED: tested %a and %A'
 end program demo_fmtdate
