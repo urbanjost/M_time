ee6fcf815dfd8c4ecc308196dc62521bfb56d38f
urbanjost <urbanjost@comcast.net>
2025-03-31 12:31:40 +0000

    Deploying to gh-pages from  @ b720bbba1ecdb8093045928a33eae786f76b534b 🚀

--- src/month.f90
+++ src/month.f90
@@ -166,7 +166,7 @@ help_text=[ CHARACTER(LEN=128) :: &
 '@(#)HOME PAGE:      http://www.urbanjost.altervista.org/index.html>',&
 '@(#)LICENSE:        Public Domain. This is free software: you are free to change and redistribute it.>',&
 '@(#)                There is NO WARRANTY, to the extent permitted by law.>',&
-'@(#)COMPILED:       2025-02-19 16:50:09 UTC-300>',&
+'@(#)COMPILED:       2025-03-28 13:27:15 UTC-240>',&
 '']
    WRITE(*,'(a)')(trim(help_text(i)(5:len_trim(help_text(i))-1)),i=1,size(help_text))
    stop ! if --version was specified, stop
--- src/today.f90
+++ src/today.f90
@@ -111,7 +111,7 @@ help_text=[ CHARACTER(LEN=128) :: &
 '@(#)DESCRIPTION:    output current time for uses such as file suffixes.>',&
 '@(#)VERSION:        1.0, 2009, 1.0.1 2024>',&
 '@(#)AUTHOR:         John S. Urban>',&
-'@(#)COMPILED:       2025-02-19 16:50:09 UTC-300>',&
+'@(#)COMPILED:       2025-03-28 13:27:15 UTC-240>',&
 '']
    WRITE(*,'(a)')(trim(help_text(i)(5:len_trim(help_text(i))-1)),i=1,size(help_text))
    stop ! if --version was specified, stop
